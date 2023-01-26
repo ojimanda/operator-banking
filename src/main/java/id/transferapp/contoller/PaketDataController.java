cc73d7cdb06904d8540d38580e05a6ebd312534f
yozimanda <yozimanda11@gmail.com>
2023-01-26 09:00:28 +0700

    add dynamic option

--- src/main/java/id/transferapp/contoller/PaketDataController.java
+++ src/main/java/id/transferapp/contoller/PaketDataController.java
@@ -3,6 +3,8 @@ package id.transferapp.contoller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,37 +16,62 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import id.transferapp.entity.InvoicepaketData;
 import id.transferapp.entity.PaketData;
+import id.transferapp.entity.ProviderCard;
 import id.transferapp.service.admin.PaketDataService;
+import id.transferapp.service.admin.ProviderCardService;
 import id.transferapp.service.operator.InvoicePaketDataService;
-
 
 @Controller
 @RequestMapping("/operator/paketdata")
 public class PaketDataController {
-	
+
 	@Autowired
 	InvoicePaketDataService invoicePaketDataService;
-	
+
 	@Autowired
 	PaketDataService paketDataService;
+
+	@Autowired
+	ProviderCardService providerCardService;
 
 	@GetMapping
 	public String index(Model model) {
 		List<PaketData> listPaket = this.paketDataService.getAllPaketData();
-		model.addAttribute("paket", listPaket);
+		List<ProviderCard> listProvider = providerCardService.getAllProvider();
+
+		JSONArray parent = new JSONArray();
+		try {
+			for (PaketData paketData : listPaket) {
+				JSONObject child = new JSONObject();
+				child.put("provider", paketData.getProviderName().getName());
+				child.put("providerId", paketData.getProviderName().getId());
+				child.put("name", paketData.getName());
+				child.put("price", paketData.getPrice());
+				parent.put(child);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		model.addAttribute("listPaket", listPaket);
+		model.addAttribute("providers", listProvider);
+		model.addAttribute("detailPaket", parent);
 		return "paketdata";
 	}
-	
-	@PostMapping("/save")
-	public @ResponseBody String save(@PathVariable("namaProvider")String namaProvider, @PathVariable("jenisPaket")String jenisPaket, @PathVariable("harga")double harga,
-			@PathVariable("noRek")String noRek, @PathVariable("phoneNum")String phoneNum, RedirectAttributes redirectAttributes) {
-		InvoicepaketData invoice = new InvoicepaketData();
-		invoice.setNamaProvider(namaProvider);
-		invoice.setJenisPaket(jenisPaket);
-		invoice.setPrice(harga);
-		invoice.setNoRek(noRek);
-		invoice.setPhoneNum(phoneNum);
-		this.invoicePaketDataService.save(invoice);
-		return "redirect:/operator/paketdata";
-	}
+
+	// @PostMapping("/save")
+	// public @ResponseBody String save(@PathVariable("namaProvider")String
+	// namaProvider, @PathVariable("jenisPaket")String jenisPaket,
+	// @PathVariable("harga")double harga,
+	// @PathVariable("noRek")String noRek, @PathVariable("phoneNum")String phoneNum,
+	// RedirectAttributes redirectAttributes) {
+	// InvoicepaketData invoice = new InvoicepaketData();
+	// invoice.setNamaProvider(namaProvider);
+	// invoice.setJenisPaket(jenisPaket);
+	// invoice.setPrice(harga);
+	// invoice.setNoRek(noRek);
+	// invoice.setPhoneNum(phoneNum);
+	// this.invoicePaketDataService.save(invoice);
+	// return "redirect:/operator/paketdata";
+	// }
 }
